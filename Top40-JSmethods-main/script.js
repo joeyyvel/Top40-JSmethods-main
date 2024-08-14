4334bb74cea3484d8570bfeae57cacfd9251aa4e
joeyboi <66668752+joeyyvel@users.noreply.github.com>
2024-08-14 13:05:48 +0800

    August 14 2024 1:06pm

--- Top40-JSmethods-main/script.js
+++ Top40-JSmethods-main/script.js
@@ -1,12 +1,21 @@
-let heart = document.getElementsByClassName("fa-heart-circle-plus");
+// function heart likes, counting people likes the page.
+let heart = document.getElementById("myID");
 let likesID = document.getElementById("likesID");
+//function counting likes from the users
+function counting() {
+  let count = 0;
+  function truncate() {
+    count++;
+    console.log(count);
+    likesID.innerHTML = count;
+  }
+  return { truncate };
+}
+// creating objects to call the inner function
+let obj = counting();
+obj.truncate();
+obj.truncate();
 
 function funcLikes() {
-  let val = 0;
-
-  function inner() {
-    val++;
-    likesID.innerText = val;
-  }
-  inner();
+  obj.truncate();
 }
