9e2b829c06c0853a83f77cda475d10796d35b344
joeyboi <66668752+joeyyvel@users.noreply.github.com>
2024-08-30 14:56:54 +0800

    August 30 2024, 2:57pm

--- Top40-JSmethods-main/script.js
+++ Top40-JSmethods-main/script.js
@@ -13,11 +13,58 @@ function counting() {
   return { truncate };
 }
 
-// creating objects to call the inner function
+//----------- creating objects to call the inner function
 let obj = counting();
 obj.truncate();
 obj.truncate();
 
 function funcLikes() {
   obj.truncate();
+}
+
+//----------- save icon by the user's count-----------
+function functionShow() {
+  let popup = document.getElementById("myPopup");
+  popup.classList.add("show");
+}
+
+function functionHide() {
+  let popup = document.getElementById("myPopup");
+  popup.classList.remove("show");
+}
+
+//-----------------------------comments section-----------
+
+let commentsID = document.getElementById("commentsID");
+
+//function for counting user comments
+
+function commenting() {
+  let count = 0;
+  function commentClosure() {
+    count++;
+    console.log(count);
+    commentsID.innerHTML = count;
+  }
+
+  return { commentClosure };
+}
+
+let commentObject = commenting();
+commentObject.commentClosure();
+commentObject.commentClosure();
+
+function functionCommentCount() {
+  commentObject.commentClosure();
+}
+
+//-------------show and hide popup comment----------------
+function functionShowCommentResult() {
+  let showPop = document.getElementById("myPopTwo");
+  showPop.classList.add("showComment");
+}
+
+function functionHideComment() {
+  let hidePop = document.getElementById("myPopTwo");
+  hidePop.classList.remove("showComment");
 }
